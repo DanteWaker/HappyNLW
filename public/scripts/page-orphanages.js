fd89b20aad442bbd73bce0aa69df6773caaeb5ec
JOSE DENNER LIRA DO NASCIMENTO <DanteWaker@users.noreply.github.com>
2020-10-15 01:56:24 -0300

    Segundo Dia NLW
    Segundo dia atrasado do NLW, mas está aqui.

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,35 @@
+
+//create map
+const map = L.map("mapid").setView([-9.7539926,-36.6595656], 15.3);
+
+//create and add tilelayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+
+const popup = L.popup({
+    closeOnClose: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" ></a>')
+
+//create and add marker
+L.marker([-9.7535093,-36.6610005], {icon})
+    .addTo(map)
+    .bindPopup(popup)
+    
+
+L.marker([-9.7566532,-36.6544554], {icon})
+    .addTo(map)
+    .bindPopup(popup)
+    
